0605b215a47bcf7390050ceee9dc2fcc839f7073
Kaeslva (ignorebyy) <115596741+ignorebyy@users.noreply.github.com>
2022-12-24 20:36:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+rom os import system as sy
+try:
+    from fake_email import Email
+except ModuleNotFoundError:
+    sy('pip install fake-email')
+
+sy('clear')
+print('Free temp-mail used pip module (fake-email)\nCreate By Kaeslva Ignorebyy')
+mail = Email().Mail()
+print('\nYour temp-mail : '+ mail['mail'])
+print('Waiting Message...\n')
+while True:
+    mess = Email(mail['session']).inbox()
+    if mess:
+        print('From : '+ mess['from'] +' | '+ mess['name'])
+        print('Subject : '+ mess['topic'])
+        print('Message : '+ mess['message'])
+        break
